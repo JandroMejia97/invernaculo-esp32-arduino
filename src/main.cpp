81bb73683ad1c690acd897adba6dcbbbda6f1012
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2023-02-07 16:03:58 -0300

    Trying to use freeRTOs

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,22 @@
 #include <HardwareSerial.h>
 #include "wifi_credentials.h"
 
+// Baud rate to communicate with the EDU-CIAA
+#define BAUD_RATE 115200
 // Serial port to communicate with the EDU-CIAA
-#define UART 2
+#define DATA_UART 2
 // Serial port pins
 #define SERIAL_RX 16
 #define SERIAL_TX 17
+
+/*
+ * Frequency to publish the data to the Ubidots MQTT broker.
+ * The data is published every FREQUENCY_TO_PUBLISH minutes, after
+ * the last data was published or the last time the data was read from the UART.
+ * For testing purposes, you can set this value to 1 (or minor).
+ */
+#define FREQUENCY_TO_PUBLISH_IN_M 10
+#define FREQUENCY_TO_PUBLISH_IN_MS (FREQUENCY_TO_PUBLISH_IN_M * 60 * 1000)
 
 typedef enum {
   ESP_INIT,
@@ -45,7 +56,7 @@ const char *WIFI_PASSWORD = WIFI_PASSWORD_SECRETS;
 // Ubidots TOKEN
 const char *UBIDOTS_TOKEN = UBIDOTS_TOKEN_SECRETS;
 // Device label
-const char *DEVICE_LABEL = "esp32-edu-ciaa";
+const char *DEVICE_LABEL = DEVICE_LABEL_SECRETS;
 // Variable labels
 const char *VARIABLE_LABELS[4] = {"temperature", "air_humidity", "light", "soil_humidity"};
 
@@ -56,10 +67,10 @@ String message;
 Ubidots client(UBIDOTS_TOKEN);
 
 unsigned long timer;
-const int PUBLISH_FREQUENCY = 15000;
+const int PUBLISH_FREQUENCY_IN_MS = FREQUENCY_TO_PUBLISH_IN_MS;
 
 // Hardware serial port to communicate with the EDU-CIAA
-HardwareSerial SerialPort(UART);
+HardwareSerial SerialPort(DATA_UART);
 
 /**
  * @brief Callback function to handle the data received from the MQTT broker
@@ -71,15 +82,20 @@ HardwareSerial SerialPort(UART);
 void callback(char *topic, byte *payload, unsigned int length);
 
 /**
+ * @brief Function to publish the data to the Ubidots MQTT broker
+ */
+void publishData(void *pvParameters);
+
+/**
  * @brief Function to handle the data received from the UART
  */
 void uartHandler();
 
 void setup() {
   // Set software serial baud to 115200;
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
   // Set hardware serial baud to 115200;
-  SerialPort.begin(115200, SERIAL_8N1, SERIAL_RX, SERIAL_TX);
+  SerialPort.begin(BAUD_RATE, SERIAL_8N1, SERIAL_RX, SERIAL_TX);
   // Connecting to a WiFi network
   client.connectToWifi(WIFI_SSID, WIFI_PASSWORD);
   // Connecting to a mqtt broker
@@ -88,6 +104,25 @@ void setup() {
   client.setup();
   client.reconnect();
   timer = millis();
+
+  /* TaskHandle_t publishDataHandle = NULL;
+
+  xTaskCreatePinnedToCore(
+    publishData, // Task function.
+    "publishData", // name of task.
+    10000, // Stack size of task
+    NULL, // parameter of the task
+    1, // priority of the task
+    &publishDataHandle, // Task handle to keep track of created task
+    0
+  ); // pin task to core 0
+
+  if (publishDataHandle == NULL) {
+    Serial.println("Error creating publishData task");
+  } else {
+    Serial.println("publishData task created");
+    vTaskDelete(publishDataHandle);
+  } */
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -136,7 +171,7 @@ void uartHandler() {
     client.add(variableLabel, value);
     long diff = millis() - timer;
     diff = diff < 0 ? -diff : diff;
-    if (diff > PUBLISH_FREQUENCY) {
+    if (diff > PUBLISH_FREQUENCY_IN_MS) {
       // Publish the data to the Ubidots MQTT broker
       client.publish(DEVICE_LABEL);
       timer = millis();
@@ -147,5 +182,15 @@ void uartHandler() {
     ESP_STATUS = ESP_DATA_ERROR;
   }
   message = "";
+}
+
+void publishData(void *params) {
+  const TickType_t xDelay = PUBLISH_FREQUENCY_IN_MS / portTICK_PERIOD_MS;
+  while (1) {
+    Serial.printf("ESP_STATUS: %s\r\n", ESP_GetErrorAsString(ESP_STATUS).c_str());
+    // Publish the data to the Ubidots MQTT broker
+    client.publish(DEVICE_LABEL);
+    vTaskDelay(xDelay);
+  }
 }
 
